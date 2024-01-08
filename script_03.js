bf48635e272fc4f347dfb7c1b605a68286f33cfc
ikramiste <ikrame.saadi@gmail.com>
2024-01-08 16:51:03 +0100

    done until 7

--- script_03.js
+++ script_03.js
@@ -9,3 +9,33 @@ const users = [
   const averageRevenuePerUser = users.reduce((totalRevenue, user) => totalRevenue + user.revenue, 0) / users.length;
   console.log(`Average revenue per user: ${averageRevenuePerUser} cents`);
   
+  // Question 2
+  const usersWithRevenue = users.filter(user => user.revenue > 0);
+  const percentageWithRevenue = (usersWithRevenue.length / users.length) * 100;
+  console.log(`Percentage of users with revenue: ${percentageWithRevenue}%`);
+  
+  // Question 3
+  const averageRevenueWithRevenue = usersWithRevenue.reduce((totalRevenue, user) => totalRevenue + user.revenue, 0) / usersWithRevenue.length;
+  console.log(`Average revenue per user with revenue: ${averageRevenueWithRevenue} cents`);
+  
+  // Question 4
+  const totalRevenue = users.reduce((totalRevenue, user) => totalRevenue + user.revenue, 0);
+  console.log(`Total revenue: ${totalRevenue} cents`);
+  
+  // Question 5
+  const usersInFrance = users.filter(user => user.country === 'France');
+  console.log(`Number of users in France: ${usersInFrance.length}`);
+  
+  // Question 6
+  const payingUsersInFrance = usersInFrance.filter(user => user.revenue > 0);
+  console.log(`Number of paying users in France: ${payingUsersInFrance.length}`);
+  
+  // Question 7
+  const topCountries = ['Germany', 'United States', 'France', 'Great Britain'];
+  const revenueByCountry = topCountries.reduce((revenueByCountry, country) => {
+    revenueByCountry[country] = users.filter(user => user.country === country).reduce((totalRevenue, user) => totalRevenue + user.revenue, 0);
+    return revenueByCountry;
+  }, {});
+  
+  console.log('Revenue by country:', revenueByCountry);
+  
